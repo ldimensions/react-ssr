abaccba9d384353a3ce803c7e2de08dd1d1e968b
Lindo Sebastian <lindosebastian@gmail.com>
2020-07-08 19:00:04 -0500

    changes

--- build/bundle.js
+++ build/bundle.js
@@ -76,16 +76,21 @@ module.exports = require("react");
 "use strict";
 
 
-var express = __webpack_require__(2);
-var React = __webpack_require__(0);
-var renderToString = __webpack_require__(3).renderToString;
-var Home = __webpack_require__(4).default;
+var _express = __webpack_require__(2);
 
-var app = express();
+var _express2 = _interopRequireDefault(_express);
+
+var _renderer = __webpack_require__(5);
+
+var _renderer2 = _interopRequireDefault(_renderer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+app.use(_express2.default.static("public"));
 
 app.get("/", function (req, res) {
-  var content = renderToString(React.createElement(Home, null));
-  res.send(content);
+  res.send((0, _renderer2.default)());
 });
 
 app.listen(3000, function () {
@@ -125,11 +130,40 @@ var Home = function Home() {
   return _react2.default.createElement(
     "div",
     null,
-    "Home Component !!"
+    "Home Component !!!!@"
   );
 };
 
 exports.default = Home;
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(3);
+
+var _Home = __webpack_require__(4);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+    var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
+
+    return "\n        <html>\n        <head></head>\n        <body>\n            <div id=\"root\"></div>\n            <script src=\"bundle.js\"></script>\n        </body>\n        </html>\n    ";
+};
 
 /***/ })
 /******/ ]);
